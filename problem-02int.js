390c1bb0a382543fe5e2c9eb559dc3af49a161d6
Ivan Hui <vanhui@ucdavis.edu>
2020-07-01 12:23:09 -0700

    in progress

---
+++ problem-02int.js
@@ -0,0 +1,18 @@
+var buildTree = function(preorder, inorder) {
+    //i:2 arrays
+    //o:binary tree
+    
+    //recursive function
+        //find out first preorderarray that matches with inorder
+            
+    const recurse = function(preorder, inorder) {
+        //find first number in preoder that is in inorder
+        let root = preorder[0]
+        
+        //build the node
+        
+        //recurse left node with number left of inorder
+        //recurse right node with number right of inorder
+        
+    }
+};
